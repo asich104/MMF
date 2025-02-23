58b3cf0ba72fbdcd5a6c0b65c62b0b9f8f0be575
Hamza Asic <hamzaasic08@gmail.com>
2025-02-23 18:03:58 +0100

    Create zadatak_1.py

---
+++ zadatak_1.py
@@ -0,0 +1,7 @@
+print("Dobro došli u tajni Kod!")
+color = str(input("Unesite svoju omiljenu boju: "))
+number = int(input("Unesite omiljeni broj: "))
+animal = str(input("Unesite naziv životinje: "))
+number += 5
+
+print(f"Vaša šifra je: {color.upper()}_{number}_{animal.upper()}. Dobro čuvajte ovaj tajni kod!")
